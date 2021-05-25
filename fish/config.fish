1ca76d1f749b364447b521e95292a669043cad84
109149 <109149qwe@gmail.com>
2021-05-25 14:41:07 +0400

    fish WTFFFF

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,8 @@
 set -g -x fish_greeting ''
-set -g -x fish_greeting ''
+# set -g -x fish_greeting ''
 
 # exit on <c-d>
 bind \cd 'exit'
-# fuzzy search on <c-f> (-M cuz vi binding is enabled)
-bind -M insert \cf 'fzf --height=20% --border=rounded --margin=1,1,1,1 --pointer="->" --color=bw'
 
 # vi stuff
 # <c-c> clears input now
@@ -24,3 +22,46 @@ starship init fish | source
 
 set -x DENO_INSTALL ~/.deno
 set -x PATH $DENO_INSTALL/bin:$PATH
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# ====> fzf
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# fuzzy search on <c-f> (-M cuz vi binding is enabled)
+# --preview="less {}"
+# --bind "enter:execute(less {})"
+
+# fuzzy find directories and cd into it
+bind -M insert \cf 'cd (fdfind -t d . --exclude node_modules --exclude target "/root/109149/senior/" | fzf); commandline -f repaint'
+# fuzzy find directories, cd into it and start tmux session
+# bind -M insert \ct 'cd (fdfind -t d . --exclude node_modules --exclude target "/root/109149/senior/" | fzf) && tmux_session.sh'
+# bind -M insert \ct 'cd (fdfind -t d . --exclude node_modules --exclude target "/root/109149/senior/" | fzf) && yes "" | tmux_session.sh; commandline -f repaint'
+# bind -M insert \ct 'cd (fdfind -t d . --exclude node_modules --exclude target "/root/109149/senior/" | fzf) && yes "" | tmux_session.sh'
+bind -M insert \ct t
+
+function t 
+  while true
+    read -l -P 'Do you want to continue? [y/N] ' confirm
+
+    switch $confirm
+      case Y y
+        return 0
+      case '' N n
+        return 1
+    end
+  end
+end
+
+# bind -M insert "ç" fzf-cd-widget
+
+set -x FZF_DEFAULT_OPTS '--cycle --height=50% --border=rounded --margin=1,1,1,1 --pointer="->" --color=16'
+# set -x FZF_ALT_C_COMMAND 'fd -t d . "/root/109149/senior/mit"'
+
+# if not set --query FZF_ALT_C_COMMAND
+#   set --global --export FZF_ALT_C_COMMAND 'fd -t d . "/root/109149/senior/mit"'
+# end
+
+# if not set --query FZF_DEFAULT_OPTS
+#   set --global --export FZF_DEFAULT_OPTS '--cycle --height=50% --border=rounded --margin=1,1,1,1 --pointer="->" --color=16'
+# end
+
