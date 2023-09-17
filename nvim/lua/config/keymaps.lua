aacc0707f68f6d91dc5d33dbf659411a553e39c8
109149qwe <109149qwe@gmail.com>
2023-09-17 15:25:45 +0400

    chore: nvim

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,93 @@
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+---- This file is automatically loaded by lazyvim.config.init
+local Util = require("lazyvim.util")
+
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    if opts.remap and not vim.g.vscode then
+      opts.remap = nil
+    end
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+map("n", "<leader>q", ":q<cr>", { desc = "Close", remap = true })
+map("n", "<leader>aq", ":qa<cr>", { desc = "Close all", remap = true })
+map("n", "<leader>w", ":w<cr>", { desc = "Save", remap = true })
+map("n", "<leader>aw", ":wa<cr>", { desc = "Save all", remap = true })
+
+-- windows
+map("n", "<leader>pw", "<C-W>p", { desc = "Previous window", remap = true })
+-- map("n", "<leader>wd", "<C-W>c", { desc = "Delete window", remap = true })
+map("n", "<leader>-", "<C-W>s", { desc = "Split window below", remap = true })
+map("n", "<leader>\\", "<C-W>v", { desc = "Split window right", remap = true })
+map("n", "<leader>h", "<cmd>wincmd h<cr>", { desc = "Go to left window" })
+map("n", "<leader>j", "<cmd>wincmd j<cr>", { desc = "Go to lower window" })
+map("n", "<leader>k", "<cmd>wincmd k<cr>", { desc = "Go to upper window" })
+map("n", "<leader>l", "<cmd>wincmd l<cr>", { desc = "Go to right window" })
+
+-- Resize window using <ctrl> arrow keys
+map("n", "<C-k>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+map("n", "<C-j>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+map("n", "<C-l>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+map("n", "<C-h>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+
+-- Move Lines
+map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+
+-- tabs
+map("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last Tab" })
+map("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First Tab" })
+map("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
+map("n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+map("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
+map("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+
+-- -- buffers
+-- if Util.has("bufferline.nvim") then
+--   map("n", "<S-h>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
+--   map("n", "<S-l>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
+--   map("n", "[b", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
+--   map("n", "]b", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
+-- else
+--   map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+--   map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+--   map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+--   map("n", "]b", "<cmd>bnext<cr>", { desc = "Next buffer" })
+-- end
+-- map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+-- map("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+--
+-- floating terminal
+local lazyterm = function()
+  Util.float_term(nil, { cwd = Util.get_root() })
+end
+map("n", "<leader>t", lazyterm, { desc = "Terminal (root dir)" })
+map("n", "<leader>T", function()
+  Util.float_term()
+end, { desc = "Terminal (cwd)" })
+-- map("n", "<c-/>", lazyterm, { desc = "Terminal (root dir)" })
+-- map("n", "<c-_>", lazyterm, { desc = "which_key_ignore" })
+
+-- Terminal Mappings
+-- map("t", "<C-h>", "<cmd>wincmd h<cr>", { desc = "Go to left window" })
+-- map("t", "<C-j>", "<cmd>wincmd j<cr>", { desc = "Go to lower window" })
+-- map("t", "<C-k>", "<cmd>wincmd k<cr>", { desc = "Go to upper window" })
+-- map("t", "<C-l>", "<cmd>wincmd l<cr>", { desc = "Go to right window" })
+-- map("t", "<C-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+-- map("t", "<c-_>", "<cmd>close<cr>", { desc = "which_key_ignore" })
+
+-- -- highlights under cursor
+-- if vim.fn.has("nvim-0.9.0") == 1 then
+--   map("n", "<leader>ui", vim.show_pos, { desc = "Inspect Pos" })
+-- end
