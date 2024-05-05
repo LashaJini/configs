ba9f7459f27da1e3bb36a9b3003f85e35741ddec
Lasha Jini <lasha.jini@toptal.com>
2024-05-05 11:35:50 +0400

    chore: k8s

--- fish/config.fish
+++ fish/config.fish
@@ -33,7 +33,8 @@ set -U fish_user_paths $HOME/.rvm/bin $fish_user_paths
 pyenv init - | source
 
 # aliases
-alias k="kubectl"
+# alias k="kubectl"
+alias k="minikube kubectl --"
 alias d="docker"
 alias pgo-cd="cd $HOME/ws/github/wholesome-ghoul/playground/go"
 
