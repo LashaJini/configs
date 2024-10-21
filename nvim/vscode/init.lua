383a9255c987335cb6413c14d7e1009634e61f7c
Lasha Jini <lasha.jini@gmail.com>
2024-10-21 15:57:19 +0400

    chore: nvim vscode

--- nvim/vscode/init.lua
+++ nvim/vscode/init.lua
@@ -1,21 +1,9 @@
 vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, noremap = true })
--- vim.g.mapleader = "<Space>"
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>R", ":source $HOME/.config/nvim/vscode/init.lua<cr>", { silent = true })
 
--- vim.keymap.set("n", "<leader>\\", ":vsplit<cr>", { noremap = true })
--- vim.keymap.set("n", "<leader>-", ":split<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>w", ":w<CR>", { noremap = true, silent = true })
-
 local vscode = require("vscode")
 print(vscode.call("_ping")) -- outputs: pong
--- print(vscode.call("_ping")) -- outputs: pong
-
--- vim.keymap.set({ "n", "x" }, "<leader>r", function()
---   vscode.with_insert(function()
---     vscode.action("editor.action.refactor")
---   end)
--- end)
 
 vim.keymap.set("n", "<leader>dn", function()
   vscode.action("editor.action.marker.next")
@@ -29,14 +17,28 @@ vim.keymap.set("n", "<leader>rn", function()
   vscode.action("editor.action.rename")
 end)
 
+--
+-- FILES
+--
+vim.keymap.set("n", "<leader>w", ":w<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>q", function()
   vscode.action("workbench.action.closeActiveEditor")
+end)
+
+--
+-- WINDOWS
+--
+vim.keymap.set("n", "<leader>-", function()
+  vscode.action("workbench.action.splitEditorDown")
 end)
 
 vim.keymap.set("n", "<leader>\\", function()
   vscode.action("workbench.action.splitEditorRight")
 end)
 
-vim.keymap.set("n", "<leader>-", function()
-  vscode.action("workbench.action.splitEditorDown")
+--
+-- SIDEBAR
+--
+vim.keymap.set("n", "<leader>nn", function()
+  vscode.action("workbench.action.toggleSidebarVisibility")
 end)
