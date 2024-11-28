7d48cc029ac8e7e76bff3ed7d4e514461f622041
Lasha Jini <lasha.jini@gmail.com>
2024-11-28 16:20:40 +0400

    chore: vscode yank

--- nvim/vscode/init.lua
+++ nvim/vscode/init.lua
@@ -1,6 +1,17 @@
 vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, noremap = true })
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>R", ":source $HOME/.config/nvim/vscode/init.lua<cr>", { silent = true })
+-- sends the yanked text to the system clipboard
+vim.keymap.set({ "n", "x" }, "y", '"+y', { noremap = true, silent = true })
+-- hightlights yanked text
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight_yank", {}),
+  desc = "Hightlight selection on yank",
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+  end,
+})
 
 local vscode = require("vscode")
 print(vscode.call("_ping")) -- outputs: pong
